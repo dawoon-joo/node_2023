a3fef7ee3d32403f5f824e3aa76ac14f13d33dcc
dawoon-joo <wnekdns5015@gmail.com>
2023-01-11 17:04:26 +0900

    프론트에서 요청시 서버에서 요청을 받아 다시 응답객체를 프론트로 전달

--- node/index.js
+++ node/index.js
@@ -7,6 +7,10 @@ const port = 5000;
 //express에서 react의 build폴더까지의 경로를 static으로 지정
 app.use(express.static(path.join(__dirname, '../react/build')));
 
+//클라이언트에서 보내는 데이터를 받도록 설정(body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //MongoDB접속
 app.listen(port, () => {
 	mongoose
@@ -15,10 +19,16 @@ app.listen(port, () => {
 		.catch((err) => console.log(err));
 });
 
+//기본 라우터 설정
 app.get('/', (req, res) => {
 	res.sendFile(path.join(___dirname, '../react/build/index.html'));
 });
-
 app.get('*', (req, res) => {
 	res.sendFile(path.join(___dirname, '../react/build/index.html'));
+});
+
+//react로 부터 받은 요청처리
+app.post('/api/send', (req, res) => {
+	console.log(req.body);
+	res.json({ success: true, resualt: req.body.name + '2' });
 });
--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,28 @@
+import axios from 'axios';
+import { useEffect, useMemo } from 'react';
+
 function App() {
+	//전송 값을 useMemo로 메모이제이션
+	const item = useMemo(() => {
+		return {
+			name: 'David',
+		};
+	}, []);
+
+	useEffect(() => {
+		//axios로 /api/send로 item객체를 서버쪽에 전달
+		axios
+			.post('/api/send', item)
+			.then((res) => {
+				//서버쪽에서 응답이 성공적으로 넘어오면 넘어온 값을 콘솔 출력
+				console.log(res);
+			})
+			.catch((err) => {
+				//실패시 에러 내용 출력
+				console.log(err);
+			});
+	}, [item]);
+
 	return (
 		<div className='App'>
 			<h1>React</h1>
