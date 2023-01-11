a0f94152fcdf83a2a31841adc35cbef7f8719597
dawoon-joo <wnekdns5015@gmail.com>
2023-01-11 16:06:37 +0900

    몽고디비 연결

--- node/index.js
+++ node/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-	res.send('Hellow World22');
+//express에서 react의 build폴더까지의 경로를 static으로 지정
+app.use(express.static(path.join(__dirname, '../react/build')));
+
+//MongoDB접속
+app.listen(port, () => {
+	mongoose
+		.connect('mongodb+srv://dawoon:ekdns0804@cluster0.1a6heit.mongodb.net/?retryWrites=true&w=majority')
+		.then(() => console.log(`Server app listening on Port ${port} width MongoDB`))
+		.catch((err) => console.log(err));
 });
 
-app.listen(port, () => {
-	console.log(`Server app listening on Port ${port}`);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(___dirname, '../react/build/index.html'));
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(___dirname, '../react/build/index.html'));
 });
