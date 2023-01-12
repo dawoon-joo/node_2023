7ad4a71fd15dd2d5c43c3791ea56ed661a37d66d
dawoon-joo <wnekdns5015@gmail.com>
2023-01-12 14:41:20 +0900

    글 고유번호를 params를통해 상세페이지 컴포넌트로 전달

--- react/src/community/List.js
+++ react/src/community/List.js
@@ -1,19 +1,34 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function List() {
+	const [List, setList] = useState([]);
 	useEffect(() => {
 		axios
 			.post('/api/read')
 			.then((res) => {
 				if (res.data.success) {
 					console.log(res.data.communityList);
+					setList(res.data.communityList);
 				}
 			})
 			.catch((err) => console.log(err));
 	}, []);
-	return <Layout name={'List'}>List</Layout>;
+	return (
+		<Layout name={'List'}>
+			{List.map((post) => {
+				return (
+					<article key={post._id}>
+						<h2>
+							<Link to={`/detail/${post.communityNum}`}>{post.title}</Link>
+						</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default List;
